c16a53ee9faf5a16d015cfe8f70b497de0e0b444
akadashiso <akadashiso@gmail.com>
2021-02-16 10:47:03 +0000

    顧客/管理者のルーティング分け

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
+
+  devise_for :admins, controllers: {
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords',
+    registrations: 'admins/registrations'
+  }
+
+  devise_for :customers, controllers: {
+    sessions:      'customers/sessions',
+    passwords:     'customers/passwords',
+    registrations: 'customers/registrations'
+  }
+
 end
