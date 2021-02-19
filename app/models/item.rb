c64240b6ed8de3ed2046255e42205f18fa3122ef
25093yukinoji <ten.yuki.0601@gmail.com>
2021-02-19 14:15:37 +0000

    #4 #5 Add

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,11 @@
 class Item < ApplicationRecord
-    
+
+    has_many :cart_items
     attachment :image
-    
-    
+
+
     #validates uniquenss重複を防ぐ inclusion属性が含まれているか確認する
-    
+
     validates :name, presence: true, uniqueness: true
     validates :introduction, presence: true
     #validates :genre_id, presence: true
