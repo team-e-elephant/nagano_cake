7334bfb975779b8642718d436f5c8fc37460abd1
watanabeyugo <watanabe98765432@gmail.com>
2021-02-20 10:36:51 +0000

    #19#20#21

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,14 @@
 class Item < ApplicationRecord
 
     has_many :cart_items
+    belongs_to :genre
     attachment :image
 
 
     #validates uniquenss重複を防ぐ inclusion属性が含まれているか確認する
-
     validates :name, presence: true, uniqueness: true
     validates :introduction, presence: true
-    #validates :genre_id, presence: true
+    validates :genre_id, presence: true
     validates :price,  presence: true
     validates :is_active, inclusion: { in: [true,false] }
 end
