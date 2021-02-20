82c0b6fc2efe0a591a0ca8cbee4eea7378c8397b
akadashiso <akadashiso@gmail.com>
2021-02-20 07:47:49 +0000

    #10 #59 作成終了

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,4 +1,26 @@
 class Public::CustomersController < ApplicationController
+
   def show
+    @customer = Customer.find(current_customer.id)
   end
+
+  def edit
+    @customer = Customer.find(current_customer.id)
+  end
+
+  def update
+    @customer = Customer.find(current_customer.id)
+    if @customer.update(customer_params)
+      redirect_to my_page_path, notice: "お客様情報更新に成功しました。"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+    def customer_params
+      params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email)
+    end
+
 end
