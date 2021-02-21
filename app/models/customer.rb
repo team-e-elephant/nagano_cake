37931fcff34930a7bd25eab3d04c18ae9947872a
25093yukinoji <ten.yuki.0601@gmail.com>
2021-02-21 06:54:25 +0000

    #6#7#8 Add

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,5 +4,7 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :cart_items
+  has_many :cart_items, dependent: :destroy
+  has_many :addresses, dependent: :destroy
+
 end
