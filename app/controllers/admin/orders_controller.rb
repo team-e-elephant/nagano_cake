e4b2211983f4b887ceddde95437fcd24bc0ca829
kohey7788 <kohey.chelsea1017@gmail.com>
2021-02-22 08:32:06 +0000

    add homes

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,11 +1,11 @@
 class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
-    @order_detail = Order_detail.find(params[:id])
+    @order_detail = current_customer.order_detail
   end
 
   def update
     @order = Order.find(params[:id])
   end
-  
+
 end
