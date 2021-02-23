01bbd77c4634a7fcb300f188dd0459a9dbd06b8e
kohey7788 <kohey.chelsea1017@gmail.com>
2021-02-23 17:51:49 +0000

    finish

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,5 +1,5 @@
 class OrderDetail < ApplicationRecord
   belongs_to :order
   belongs_to :item
-  enum making_status:{"制作不可": 0, "制作待ち": 1, "製作中": 2,"制作完了": 3}
+  enum making_status: {cannot_be_started: 0, waiting_for_production: 1, in_production: 2, production_completed: 3}
 end
