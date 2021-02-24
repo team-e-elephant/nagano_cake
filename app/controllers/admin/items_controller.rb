a421cb9a2ea2f30f302aa9a58b5ad5e866f6694e
25093yukinoji <ten.yuki.0601@gmail.com>
2021-02-24 03:33:03 +0000

    Add

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,21 +1,23 @@
 class Admin::ItemsController < ApplicationController
-    
+
+    before_action :authenticate_admin!
+
     def index
         @genre = Genre.select(:name)
-        @items = Item.page(params[:page]).per(10) 
+        @items = Item.page(params[:page]).per(10)
     end
-    
+
     def show
         @item = Item.find(params[:id])
     end
-    
+
     def new
         @item = Item.new
         @genres = Genre.all
     end
-    
+
     def create
-        
+
         @genres = Genre.all
         @item = Item.new(item_params)
         if @item.save
@@ -28,7 +30,7 @@ class Admin::ItemsController < ApplicationController
     def edit
         @item = Item.find(params[:id])
     end
-    
+
     def update
       @item = Item.find(params[:id])
       if @item.update(item_params)
@@ -37,9 +39,9 @@ class Admin::ItemsController < ApplicationController
       render :edit
       end
     end
-    
+
 private
-  
+
     def item_params
         params.require(:item).permit(:genre_id,:name,:image,:introduction,:price,:is_active)
     end
--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,4 +1,7 @@
 class Admin::OrderDetailsController < ApplicationController
+
+  before_action :authenticate_admin!
+
   def update
     @order_detail = OrderDetail.find(params[:id])
     # byebug
